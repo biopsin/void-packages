523a93fa495eea1336fd1727900a5689d04c6a0a
biopsin <biopsin@tuta.io>
2025-09-11 11:16:41 +0200

    firefox-esr: update to 128.14.0

--- srcpkgs/firefox-esr/files/vendor.js
+++ srcpkgs/firefox-esr/files/vendor.js
@@ -6,6 +6,4 @@ pref("browser.shell.checkDefaultBrowser", false);
 
 // Don't disable our bundled extensions in the application directory
 pref("extensions.autoDisableScopes", 11);
-
-// The default is google and we don't have api keys for it.
-pref("geo.provider.network.url", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
+pref("extensions.shownSelectionUI", true);
